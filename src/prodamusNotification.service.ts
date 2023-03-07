d2fa64345f9873a3bef5674c6b332c2373872000
leviskay <leviathan.po4ta@gmail.com>
2023-03-07 22:52:05 +0300

    refactor: prodamusNotificationService

--- src/prodamusNotification.service.ts
+++ src/prodamusNotification.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PaymentCreatedEvent } from './events/paymentCreated.event';
 import { PaymentErrorOccuredEvent } from './events/paymentErrorOccured.event';
+import { ProdamusEvent } from './events/ProdamusEvents';
 import { SubscriptionCreatedEvent } from './events/subcriptionCreated.event';
 import { SubscriptionDeactivatedEvent } from './events/subscriptionDeactivated.event';
 import { SubscriptionFinishedEvent } from './events/subscriptionFinished.event';
@@ -8,38 +9,67 @@ import { SubscriptionRenewedEvent } from './events/subscriptionRenewed.event';
 import { UpcomingPaymentNotificationRecievedEvent } from './events/upcomingPaymentNotificationRecieved.event';
 import { ProdamusEventService } from './prodamusEventsService';
 
+const eventFactories: {
+  create: (notification: any) => ProdamusEvent<any>;
+  isApplicableForNotification: (notification: any) => boolean;
+}[] = [
+  {
+    create: SubscriptionCreatedEvent.create.bind(SubscriptionCreatedEvent),
+    isApplicableForNotification: (notification) =>
+      notification?.products && notification?.subscription,
+  },
+  {
+    create: PaymentCreatedEvent.create.bind(PaymentCreatedEvent),
+    isApplicableForNotification: (notification: any) =>
+      notification?.products && !notification.subscription,
+  },
+  {
+    create: SubscriptionRenewedEvent.create.bind(SubscriptionRenewedEvent),
+    isApplicableForNotification: (notification: any) =>
+      notification?.subscription?.action_code == 'auto_payment',
+  },
+  {
+    create: SubscriptionDeactivatedEvent.create.bind(
+      SubscriptionDeactivatedEvent,
+    ),
+    isApplicableForNotification: (notification: any) =>
+      notification?.subscription?.action_code == 'deactivation',
+  },
+
+  {
+    create: SubscriptionFinishedEvent.create.bind(SubscriptionFinishedEvent),
+    isApplicableForNotification: (notification: any) =>
+      notification?.subscription?.action_code == 'finish',
+  },
+  {
+    create: PaymentErrorOccuredEvent.create.bind(PaymentErrorOccuredEvent),
+    isApplicableForNotification: (notification: any) =>
+      notification?.subscription?.error,
+  },
+
+  {
+    create: UpcomingPaymentNotificationRecievedEvent.create.bind(
+      UpcomingPaymentNotificationRecievedEvent,
+    ),
+    isApplicableForNotification: (notification: any) =>
+      notification?.subscription?.notification_code === 'auto_payment_reminder',
+  },
+];
+
 @Injectable()
 export class ProdamusNotificationService {
   constructor(private readonly eventService: ProdamusEventService) {}
 
   async handleNotification(notification: any) {
-    console.log(notification);
-    if (notification?.products && notification.subscription) {
-      this.eventService.emit(SubscriptionCreatedEvent.create(notification));
-    } else if (notification?.products && !notification.subscription) {
-      this.eventService.emit(PaymentCreatedEvent.create(notification));
-    } else if (notification.subscription) {
-      if (notification?.subscription?.action_code == 'auto_payment') {
-        this.eventService.emit(SubscriptionRenewedEvent.create(notification));
-      } else if (notification?.subscription?.action_code == 'deactivation') {
-        this.eventService.emit(
-          SubscriptionDeactivatedEvent.create(notification),
-        );
-      } else if (notification?.subscription?.action_code == 'finish') {
-        this.eventService.emit(SubscriptionFinishedEvent.create(notification));
-      } else if (notification?.subscription?.error) {
-        this.eventService.emit(PaymentErrorOccuredEvent.create(notification));
-      } else if (
-        notification?.subscription?.notification_code ===
-        'auto_payment_reminder'
-      ) {
-        this.eventService.emit(
-          UpcomingPaymentNotificationRecievedEvent.create(notification),
-        );
-      } else {
-        console.log('Неизвестный тип уведомления');
-      }
+    const factory = eventFactories.find((factory) =>
+      factory.isApplicableForNotification(notification),
+    );
+    if (factory == undefined) {
+      return console.error('Неизвестный тип уведомления', notification);
     }
+
+    const event = factory.create(notification);
+    this.eventService.emit(event);
     return;
   }
 }
