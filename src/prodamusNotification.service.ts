afd443a2cf681457c108580c9efea22068ecba52
leviskay <leviathan.po4ta@gmail.com>
2023-03-07 15:26:03 +0300

    add ifs

--- src/prodamusNotification.service.ts
+++ src/prodamusNotification.service.ts
@@ -4,6 +4,28 @@ import { Injectable } from '@nestjs/common';
 export class ProdamusNotificationService {
   async handleNotification(notification: any) {
     console.log(notification);
+    if (notification?.products && notification.subscription) {
+      console.log('Подписка создана');
+    } else if (notification?.products && !notification.subscription) {
+      console.log('Обычный платеж');
+    } else if (notification.subscription) {
+      if (notification?.subscription?.action_code == 'auth_payment') {
+        console.log('Успешное продление');
+      } else if (notification?.subscription?.action_code == 'deactivation') {
+        console.log('Диактивация подписки');
+      } else if (notification?.subscription?.action_code == 'finish') {
+        console.log('Окончание подписки');
+      } else if (notification?.subscription?.error) {
+        console.log('Ошибка');
+      } else if (
+        notification?.subscription?.notification_code ===
+        'auto_payment_reminder'
+      ) {
+        console.log('Уведомление о предстоящем списании');
+      } else {
+        console.log('Неизвестный тип уведомления');
+      }
+    }
     return;
   }
 }
